e1359dd8cd2fb22a94bb9ff65bf72acb5d556022
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-09 01:27:04 +0200

    02-file-appender

--- 02-file-appender/src/main/java/com/infoshareacademy/loggers/App.java
+++ 02-file-appender/src/main/java/com/infoshareacademy/loggers/App.java
@@ -5,14 +5,11 @@ import org.apache.logging.log4j.Logger;
 
 public class App {
     public static void main(String[] args) {
-        Logger logger = LogManager.getLogger(App.class.getName());
+        Logger logger = LogManager.getLogger("file-appender");
 
         logger.info("This is an info message");
-
         logger.warn("This is a warning!");
-
         logger.error("Error!");
-
         logger.debug("This is a special top secret debug message only for developers");
     }
 }

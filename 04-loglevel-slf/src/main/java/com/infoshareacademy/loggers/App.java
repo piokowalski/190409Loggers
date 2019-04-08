7fe82d6d733427d13d105239089feeae123bd2f9
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-09 01:53:32 +0200

    04-loglevel-slf

---
+++ 04-loglevel-slf/src/main/java/com/infoshareacademy/loggers/App.java
@@ -0,0 +1,18 @@
+package com.infoshareacademy.loggers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(App.class.getName());
+
+        logger.info("This is an info message");
+
+        logger.warn("This is a warning!");
+
+        logger.error("Error!");
+
+        logger.debug("This is a special top secret debug message only for developers");
+    }
+}

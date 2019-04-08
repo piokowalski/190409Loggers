7b139ffb5ad0c1097932d2d6448eb2298068f57d
Marcin Kwiatkowski <m.kwiatkowski@int.pl>
2019-04-09 00:43:41 +0200

    01-loglevel

---
+++ 01-loglevel/src/main/java/com/infoshareacademy/loggers/App.java
@@ -0,0 +1,14 @@
+package com.infoshareacademy.loggers;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class App {
+    public static void main(String[] args) {
+        Logger logger = LogManager.getLogger(App.class);
+        logger.info("This is an info message");
+        logger.warn("This is a warning!");
+        logger.error("Error!");
+        logger.debug("This is a special top secret debug message only for developers");
+    }
+}
